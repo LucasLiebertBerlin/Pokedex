8300204d53b89776bc62e1c6f8933a407498834f
unknown <lucas.liebert20@gmail.com>
2024-02-19 22:26:04 +0100

    standards

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-
-
-
+async function init() {
+  loadPokemon();
+}
 
 async function loadPokemon() {
-
-    let url = 'https://pokeapi.co/api/v2/pokemon/1025';
+  for (let i = 1; i <= 10; i++) {
+    // 898
+    let pokemonId = i;
+    let url = "https://pokeapi.co/api/v2/pokemon/" + pokemonId;
     let response = await fetch(url);
     let responseAsJson = await response.json();
+    console.log("loaded Pokemon", responseAsJson);
+    let pokemonName = responseAsJson["species"]["name"];
+    console.log("pokemon name ist", pokemonName);
+  }
+}
 
-    console.log('loaded Pokemon', responseAsJson);
+function loadContent() {
+
+
+    
 }
