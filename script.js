a1e3fa373ce559028a6bcc3ff8fdc87941b80c2f
unknown <lucas.liebert20@gmail.com>
2024-02-20 03:09:51 +0100

    pokemon types loading

--- script.js
+++ script.js
@@ -1,84 +1,82 @@
-// Funktion, die beim Initialisieren der Seite aufgerufen wird
-async function init() {
-  // Lade die ersten 30 Pokémon
-  await loadInitialPokemon();
-}
-
-// Startindex für die geladenen Pokémon
+// Globale Variablen für den Start- und Endindex der geladenen Pokémon
 let start = 1;
-// Endindex für die geladenen Pokémon
 let end = 30;
 
-// Funktion zum Laden der ersten 30 Pokémon
-async function loadInitialPokemon() {
-  await loadPokemon(start, end);
+// Funktion zum Initialisieren der Seite
+async function init() {
+    await loadInitialPokemon(start, end);
 }
 
-// Funktion zum Laden von Pokémon basierend auf dem Start- und Endindex
-async function loadPokemon(start, end) {
-  for (let i = start; i <= end; i++) {
-    // Füge die Pokémon-Karte hinzu, nachdem die Daten von der API abgerufen wurden
-    addPokemonCard(await fetchPokemon(i));
+  // Funktion zum Laden der ersten Pokémon
+  async function loadInitialPokemon(startIndex, endIndex) {
+    await loadPokemon(startIndex, endIndex);
   }
-}
-
-// Funktion zum Abrufen von Pokémon-Daten von der API
-async function fetchPokemon(pokemonId) {
-  // URL zur API-Anfrage für das Pokémon
-  let url = "https://pokeapi.co/api/v2/pokemon/" + pokemonId;
-  // API-Anfrage und Rückgabe der JSON-Daten
-  let response = await fetch(url);
-  return await response.json();
-}
-
-// Funktion zum Generieren des HTML-Codes für eine Pokémon-Karte
-function generatePokemonCardHtml(pokemonName, pokemonImage) {
-  // HTML-Code für die Pokémon-Karte unter Verwendung der übergebenen Daten
-  return /*html*/ `
+  
+  // Funktion zum Laden von Pokémon basierend auf dem Start- und Endindex
+  async function loadPokemon(startIndex, endIndex) {
+    for (let i = startIndex; i <= endIndex; i++) {
+      // Füge die Pokémon-Karte hinzu, nachdem die Daten von der API abgerufen wurden
+      await addPokemonCard(i);
+    }
+  }
+  
+  // Funktion zum Abrufen von Pokémon-Daten von der API
+  async function fetchPokemonData(pokemonId) {
+    let url = "https://pokeapi.co/api/v2/pokemon/" + pokemonId;
+    let response = await fetch(url);
+    return await response.json();
+  }
+  
+  // Funktion zum Generieren des HTML-Codes für eine Pokémon-Karte
+  function createPokemonCard(pokemonName, pokemonImage, types) {
+    let typesHtml = generatePokemonCardTypesHtml(types);
+    return /*html*/ `
       <div class="pokemon-card">
           <div class="pokemon-card-headline-name">
               <h1 class="pokemon-card-headline">${pokemonName}</h1>
           </div>
           <div class="pokemon-card-main-content-wrapper">
               <div class="pokemon-card-types-container">
+                  ${typesHtml}
               </div>
               <div class="pokemon-card-image-container">
                   <img class="pokemon-card-image" src="${pokemonImage}" alt="">
               </div>
           </div>
       </div>`;
-}
-
-// Funktion zum Hinzufügen einer Pokémon-Karte zum Hauptcontainer
-function addPokemonCard(pokemonData) {
-  // Extrahiere den Namen und das Bild des Pokémon aus den Daten
-  let pokemonName = pokemonData["species"]["name"];
-  let pokemonImage = pokemonData["sprites"]["front_shiny"];
-  // Hauptcontainer für die Anzeige der Pokémon-Karten abrufen
-  let mainContentContainer = document.getElementById("mainContentPokedex");
-  // HTML-Code für die Pokémon-Karte generieren und dem Hauptcontainer hinzufügen
-  mainContentContainer.innerHTML += generatePokemonCardHtml(
-    pokemonName,
-    pokemonImage
-  );
-}
-
+  }
+  
+  // Funktion zum Generieren des HTML-Codes für die Pokémon-Typen
+  function generatePokemonCardTypesHtml(types) {
+    let typesHtml = "";
+    for (let i = 0; i < types.length; i++) {
+      let type = types[i]["type"]["name"];
+      typesHtml += /*html*/ `
+          <div><b>${type}</b></div>
+      `;
+    }
+    return typesHtml;
+  }
+  
+  // Funktion zum Hinzufügen einer Pokémon-Karte zum Hauptcontainer
+  async function addPokemonCard(pokemonIndex) {
+    let pokemonData = await fetchPokemonData(pokemonIndex);
+    let pokemonName = pokemonData["species"]["name"];
+    let pokemonImage = pokemonData["sprites"]["front_shiny"];
+    let types = pokemonData["types"];
+    let mainContentContainer = document.getElementById("mainContentPokedex");
+    mainContentContainer.innerHTML += createPokemonCard(pokemonName, pokemonImage, types);
+  }
+  
 // Funktion zum Laden weiterer Pokémon beim Klicken auf den Button
 async function loadMorePokemon() {
-  // Neuer Startindex für die nächsten 30 Pokémon
-  let newStart = end + 1;
-  // Neuer Endindex für die nächsten 30 Pokémon
-  let newEnd = end + 30;
-  // Überprüfen, ob der neue Endindex größer als die Gesamtanzahl der Pokémon ist
-  if (newEnd > 898) {
-    // Setze den Endindex auf 898, um alle verbleibenden Pokémon zu laden
-    newEnd = 898;
-  }
+    let newStart = end + 1;
+    let newEnd = end + 30;
+    if (newEnd > 898) {
+        newEnd = 898;
+    }
 
-  // Lade die nächsten Pokémon basierend auf den neuen Start- und Endindizes
-  await loadPokemon(newStart, newEnd);
-  // Aktualisiere den Startindex für zukünftige Ladevorgänge
-  start = newStart;
-  // Aktualisiere den Endindex für zukünftige Ladevorgänge
-  end = newEnd;
+    await loadPokemon(newStart, newEnd);
+    start = newStart; // Aktualisiere den Startindex
+    end = newEnd; // Aktualisiere den Endindex
 }
