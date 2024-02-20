dbff8075042558b5228bb86c87caf39e21984e4e
unknown <lucas.liebert20@gmail.com>
2024-02-20 04:38:45 +0100

    changing typ color cards

--- script.js
+++ script.js
@@ -26,12 +26,33 @@ async function init() {
     let response = await fetch(url);
     return await response.json();
   }
-  
-  // Funktion zum Generieren des HTML-Codes für eine Pokémon-Karte
-  function createPokemonCard(pokemonName, pokemonImage, types) {
+  function createPokemonCard(pokemonName, pokemonImage, types, pokemonIndex) {
     let typesHtml = generatePokemonCardTypesHtml(types);
+
+    // Map, die die Farben für jeden Pokémon-Typ definiert
+    const colorMap = {
+        'normal': 'grey',
+        'fire': 'red',
+        'water': 'blue',
+        'electric': 'yellow',
+        'grass': 'green',
+        'ice': 'lightblue',
+        'fighting': 'red',
+        'poison': 'purple',
+        'ground': 'brown',
+        'flying': 'skyblue',
+        'psychic': 'pink',
+        'bug': 'green',
+        'rock': 'grey',
+        'ghost': 'purple',
+        'dragon': 'lightblue'
+    };
+
+    // Festlegen der Farbe basierend auf dem ersten Pokémon-Typ
+    let cardColor = colorMap[types[0]["type"]["name"]] || '';
+
     return /*html*/ `
-      <div class="pokemon-card">
+      <div id="pokemonCard${pokemonIndex}" class="pokemon-card ${cardColor}">
           <div class="pokemon-card-headline-name">
               <h1 class="pokemon-card-headline">${pokemonName}</h1>
           </div>
@@ -44,15 +65,72 @@ async function init() {
               </div>
           </div>
       </div>`;
-  }
-  
+}
+
+//   }
+//   function generateCardColorFromType(types, pokemonIndex) {
+//     const pokemonCard = document.getElementById(`pokemonCard${pokemonIndex}`);
+    
+//     // Überprüfen, ob das Element gefunden wurde
+//     if (pokemonCard) {
+//         const colorMap = {
+//             'normal': 'grey',
+//             'fire': 'red',
+//             'water': 'blue',
+//             'electric': 'yellow',
+//             'grass': 'green',
+//             'ice': 'lightblue',
+//             'fighting': 'red',
+//             'poison': 'purple',
+//             'ground': 'brown',
+//             'flying': 'skyblue',
+//             'psychic': 'pink',
+//             'bug': 'green',
+//             'rock': 'grey',
+//             'ghost': 'purple',
+//             'dragon': 'lightblue'
+//         };
+
+//         const colorCount = {};
+
+//         // Bestimme die Häufigkeit jeder Farbe
+//         types.forEach(type => {
+//             const typeName = type.type.name;
+//             if (colorMap[typeName]) {
+//                 colorCount[colorMap[typeName]] = (colorCount[colorMap[typeName]] || 0) + 1;
+//             }
+//         });
+
+//         // Wähle die dominante Farbe aus
+//         let dominantColor = '';
+//         let maxCount = 0;
+//         for (const color in colorCount) {
+//             if (colorCount[color] > maxCount) {
+//                 dominantColor = color;
+//                 maxCount = colorCount[color];
+//             }
+//         }
+
+//         // Füge die CSS-Klasse für die dominante Farbe hinzu
+//         pokemonCard.classList.add(dominantColor);
+
+//         // Rückgabe der dominanten Farbe
+//         return dominantColor;
+//     }
+
+//     // Wenn das Element nicht gefunden wurde, gib null zurück
+//     return null;
+// }
+
+
+
   // Funktion zum Generieren des HTML-Codes für die Pokémon-Typen
   function generatePokemonCardTypesHtml(types) {
     let typesHtml = "";
     for (let i = 0; i < types.length; i++) {
       let type = types[i]["type"]["name"];
       typesHtml += /*html*/ `
-          <div><b>${type}</b></div>
+          <div class="pokemon-card-type"><b>${type}</b></div>
       `;
     }
     return typesHtml;
@@ -61,11 +139,12 @@ async function init() {
   // Funktion zum Hinzufügen einer Pokémon-Karte zum Hauptcontainer
   async function addPokemonCard(pokemonIndex) {
     let pokemonData = await fetchPokemonData(pokemonIndex);
+    console.log(pokemonData);
     let pokemonName = pokemonData["species"]["name"];
     let pokemonImage = pokemonData["sprites"]["front_shiny"];
     let types = pokemonData["types"];
     let mainContentContainer = document.getElementById("mainContentPokedex");
-    mainContentContainer.innerHTML += createPokemonCard(pokemonName, pokemonImage, types);
+    mainContentContainer.innerHTML += createPokemonCard(pokemonName, pokemonImage, types, pokemonIndex);
   }
   
 // Funktion zum Laden weiterer Pokémon beim Klicken auf den Button
