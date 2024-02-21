5ed69629186e1751f409abf66ddafd189a0ebf99
unknown <lucas.liebert20@gmail.com>
2024-02-21 22:52:08 +0100

    selected card background

--- script.js
+++ script.js
@@ -2,7 +2,26 @@
 let start = 1;
 let end = 30;
 let pokemonArray = [];
-
+  // Map, die die Farben für jeden Pokémon-Typ definiert
+  let colorMap = {
+    normal: "grey",
+    fire: "red",
+    water: "blue",
+    electric: "yellow",
+    grass: "green",
+    ice: "lightblue",
+    fighting: "red",
+    poison: "purple",
+    ground: "brown",
+    flying: "skyblue",
+    psychic: "pink",
+    bug: "green",
+    rock: "grey",
+    ghost: "purple",
+    dragon: "lightblue",
+    fairy: "pink",
+    dark: "black",
+  };
 // Funktion zum Initialisieren der Seite
 async function init() {
   await loadInitialPokemon();
@@ -45,32 +64,12 @@ async function fetchPokemonData(pokemonId) {
 // Funktion zum Generieren des HTML-Codes für die Pokémon-Karten
 function createPokemonCard(pokemonName, pokemonImage, types, pokemonIndex) {
   let typesHtml = generatePokemonCardTypesHtml(types);
-  let typesJsonString = JSON.stringify(types);
-  // Map, die die Farben für jeden Pokémon-Typ definiert
-  const colorMap = {
-    normal: "grey",
-    fire: "red",
-    water: "blue",
-    electric: "yellow",
-    grass: "green",
-    ice: "lightblue",
-    fighting: "red",
-    poison: "purple",
-    ground: "brown",
-    flying: "skyblue",
-    psychic: "pink",
-    bug: "green",
-    rock: "grey",
-    ghost: "purple",
-    dragon: "lightblue",
-    fairy: "pink",
-    dark: "black",
-  };
+
 
   // Festlegen der Farbe basierend auf dem ersten Pokémon-Typ
   let cardColor = colorMap[types[0]["type"]["name"]] || "";
   return /*html*/ `
-  <div onclick="openSelectedCard()" id="pokemonCard${pokemonIndex}" class="pokemon-card ${cardColor}">
+  <div onclick="openSelectedCard(${pokemonIndex})" id="pokemonCard${pokemonIndex}" class="pokemon-card ${cardColor}">
       <div class="pokemon-card-headline-name">
           <h1 class="pokemon-card-headline">${pokemonName}</h1>
       </div>
@@ -97,8 +96,60 @@ function generatePokemonCardTypesHtml(types) {
   return typesHtml;
 }
 
-function openSelectedCard() {
+function openSelectedCard(pokemonIndex) {
+
+  let selectedPokemonName = pokemonArray[pokemonIndex - 1]['name']; // -1 Da Arrays bei 0 beginnen
+  let selectedPokemonImage = pokemonArray[pokemonIndex -1]['image']; // -1 Da Arrays bei 0 beginnen
+  let selectedPokemonTypes = pokemonArray[pokemonIndex -1]['types']; // -1 Da Arrays bei 0 beginnen
+  let SelectedPokemonCardColor = colorMap[selectedPokemonTypes[0]["type"]["name"]] || "";
+  let typesHtml = generatePokemonCardTypesHtml(selectedPokemonTypes);
+
+  console.log('name', selectedPokemonTypes);
+
+  let backgroundCloseDiv = document.getElementById('closeSelectedCardBackgroundDiv');
+  backgroundCloseDiv.classList.remove('d-none');
   let selectedCardContainer = document.getElementById('selectedCardContainer');
+  selectedCardContainer.classList.remove('d-none');
+  selectedCardContainer.innerHTML = ``;
+  selectedCardContainer.innerHTML = /*html*/`
+    
+  <div class="selected-card ${SelectedPokemonCardColor}">
+    <div class="selected-card-close-button">
+        <img onclick="closeSelectedCard()" class="normal-button" src="/img/close-button.png" alt="">
+    </div>
+    <div class="selected-card-headline">
+        <h1>${selectedPokemonName}</h1>
+    </div>
+    <div class="selected-card-main">
+      <img onclick="openSelectedCard(${pokemonIndex -1})" class="normal-button" src="/img/back-button.png" alt="">
+      <div class="selected-card-main-content-wrapper">
+        <div class="selected-card-main-content">
+          <div class="selected-card-main-image">
+            <img src="${selectedPokemonImage}" alt="">
+          </div>
+          <div class="selected-card-main-types">
+            ${typesHtml}
+          </div>
+        </div>
+    </div>
+    <img onclick="openSelectedCard(${pokemonIndex +1})"  class="normal-button" src="/img/forward-button.png" alt="">
+  </div>
+  `;  
+
+let body = document.getElementById('body');
+  body.style.overflow = 'hidden';
+}
+
+function closeSelectedCard() {
+  let selectedCardContainer = document.getElementById('selectedCardContainer');
+  selectedCardContainer.classList.add('d-none');
+  
+  let backgroundCloseDiv = document.getElementById('closeSelectedCardBackgroundDiv');
+  backgroundCloseDiv.classList.add('d-none');
+
+  let body = document.getElementById('body');
+  body.style.overflow = 'auto';
+
 }
 
 // Funktion zum Laden weiterer Pokémon beim Klicken auf den Button
